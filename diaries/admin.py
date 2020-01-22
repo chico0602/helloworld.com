47783127386936c2c8a98e8e75a20701a022f247
chico <yukamori.0602@gmail.com>
2020-01-22 19:56:35 +0900

    reviews add

--- diaries/admin.py
+++ diaries/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Diary
+from .models import Diary, Category
 
 admin.site.register(Diary)
+admin.site.register(Category)
