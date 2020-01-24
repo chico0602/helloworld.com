41cc79e2b99023adefbf9ad30aa8bcd2f76e14a4
chico <yukamori.0602@gmail.com>
2020-01-24 20:43:28 +0900

    reviewhtml

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'reviews'
 
 urlpatterns = [
-
+    path('', views.review_list, name='review_list'),
+    path('detail/<str:store_name>/', views.review_detail, name='review_detail'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Review
 # Create your views here.
+
+
+def review_list(request):
+    context = {
+        'review_list': Review.objects.all().order_by('-created_at'),
+    }
+    return render(request, 'reviews/review_list.html', context)
+
+def review_detail(request, store_name):
+    review = Review.objects.get(store_name=store_name)
+    context = {
+        'review': review,
+    }
+    return render(request, 'reviews/review_detail.html', context)
