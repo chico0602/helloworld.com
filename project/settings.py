8d4ebfdf6423b274472d144bae38d1b0684d2344
chico <yukamori.0602@gmail.com>
2020-02-09 19:16:34 +0900

    [add]contact

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'myprofile.apps.MyprofileConfig',
     'diaries.apps.DiariesConfig',
     'reviews.apps.ReviewsConfig',
+    'contact.apps.ContactConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
