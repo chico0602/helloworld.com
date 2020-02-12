12e541b8e7a26f050901c43474ad4dc6c1657b99
chico <yukamori.0602@gmail.com>
2020-02-12 19:15:02 +0900

    [update]contact

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 
 class ContactForm(forms.Form):
     CATEGORIES = (
@@ -8,10 +9,24 @@ class ContactForm(forms.Form):
 
     name = forms.CharField(
         label='お名前', max_length=50,
-        required=False, help_text='※任意'
+        required=False, help_text='※任意',
     )
     email = forms.EmailField(
-        label='メールアドレス', required=False, help_text='※任意'
+        label='メールアドレス', required=False, help_text='※任意',
     )
-    text = forms.CharField(label='お問い合わせ内容', widget=forms.Textarea)
-    category = forms.ChoiceField(label='カテゴリ', choices=CATEGORIES)
+    text = forms.CharField(
+        label='お問い合わせ内容'
+    )
+    category = forms.ChoiceField(
+        label='カテゴリ', choices=CATEGORIES,
+        )
+    #category = forms.MultipleChoiceField(
+    #    label = 'カテゴリ', choices=CATEGORIES,
+    #    widget=forms.CheckboxSelectMultiple
+    #)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['placeholder'] = field.label
+
+    created_at = forms.DateField(label='日付')
